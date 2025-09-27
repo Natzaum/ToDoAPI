e9b060c0d94fcf2aee4614f5f4c11b0346383a50
Natzaum <natacezerbordignon@gmail.com>
2025-09-26 23:40:15 -0300

    feat: finalização

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import type { Request, Response, NextFunction } from "express";
+import cors from "cors";
+import morgan from "morgan";
+import helmet from "helmet";
+import taskRouter from "./routers/taskRouter";
+
+const app = express();
+
+app.use(morgan("tiny"));
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+
+app.use("/tasks", taskRouter);
+
+app.get("/health", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  res.status(500).send(error.message);
+});
+
+export default app;
